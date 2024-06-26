8b8f0e028f1bd8b1069b7081224fe2fed5fbaff6
Aom29 <127160678+Aom29@users.noreply.github.com>
2024-06-26 16:18:09 -0600

    Con un solo carrito ID auto

--- Auxiliares/SQL tienda/Creación de la base.sql
+++ Auxiliares/SQL tienda/Creación de la base.sql
@@ -14,7 +14,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema grappe1
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `grappe1` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE SCHEMA IF NOT EXISTS `grappe1` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ;
 USE `grappe1` ;
 
 -- -----------------------------------------------------
@@ -25,16 +25,13 @@ CREATE TABLE IF NOT EXISTS `grappe1`.`administrador` (
   `Nombre` VARCHAR(50) NOT NULL,
   `Apellidos` VARCHAR(50) NOT NULL,
   `Email` VARCHAR(100) NOT NULL,
-  `Contraseña` VARCHAR(8) NOT NULL,
+  `Contraseña` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id_adm`))
 ENGINE = InnoDB
 AUTO_INCREMENT = 81
-DEFAULT CHARACTER SET = utf8mb4;
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
 
-
--- -----------------------------------------------------
--- Table `grappe1`.`producto`
--- -----------------------------------------------------
 -- -----------------------------------------------------
 -- Table `grappe1`.`producto`
 -- -----------------------------------------------------
@@ -46,13 +43,13 @@ CREATE TABLE IF NOT EXISTS `grappe1`.`producto` (
   `id_categoria` INT NOT NULL,
   `Descuento` DECIMAL(10,2) NOT NULL,
   `Descripcion` TEXT NOT NULL,
+  `Disponible` TINYINT NOT NULL,
   PRIMARY KEY (`id_producto`),
-  INDEX `idx_id_categoria` (`id_categoria`)
-)
+  INDEX `id_categoria` (`id_categoria` ASC))
 ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4;
-
-
+AUTO_INCREMENT = 101
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
 
 -- -----------------------------------------------------
 -- Table `grappe1`.`agrega`
@@ -60,59 +57,17 @@ DEFAULT CHARACTER SET = utf8mb4;
 CREATE TABLE IF NOT EXISTS `grappe1`.`agrega` (
   `id_producto` INT NOT NULL,
   `id_adm` INT NOT NULL,
-  INDEX `idx_id_producto` (`id_producto`, `id_adm`),
-  INDEX `idx_id_adm` (`id_adm`),
+  INDEX `id_producto` (`id_producto` ASC, `id_adm` ASC),
+  INDEX `id_adm` (`id_adm` ASC),
   CONSTRAINT `agrega_ibfk_1`
     FOREIGN KEY (`id_producto`)
     REFERENCES `grappe1`.`producto` (`id_producto`),
   CONSTRAINT `agrega_ibfk_2`
     FOREIGN KEY (`id_adm`)
-    REFERENCES `grappe1`.`administrador` (`id_adm`)
-)
+    REFERENCES `grappe1`.`administrador` (`id_adm`))
 ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4;
-
-
-
--- -----------------------------------------------------
--- Table `grappe1`.`cliente`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `grappe1`.`cliente` (
-  `id_cliente` INT NOT NULL AUTO_INCREMENT,
-  `Nombre` VARCHAR(50) NOT NULL,
-  `Apellido` VARCHAR(50) NOT NULL,
-  `Email` VARCHAR(50) NOT NULL,
-  `Contraseña` VARCHAR(8) NOT NULL,
-  `Saldo` INT NOT NULL,
-  `Numero Tarjeta` BIGINT NOT NULL,
-  `Titular` VARCHAR(100) NOT NULL,
-  `Año Vencimiento` INT NOT NULL,
-  `Mes Vencimiento` INT NOT NULL,
-  `CVV` INT NOT NULL,
-  PRIMARY KEY (`id_cliente`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 232
-DEFAULT CHARACTER SET = utf8mb4;
-
-
--- -----------------------------------------------------
--- Table `grappe1`.`carrito`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `grappe1`.`carrito` (
-  `id_carrito` INT NOT NULL AUTO_INCREMENT,
-  `id_cliente` INT NOT NULL,
-  `Total` INT NOT NULL,
-  `Cantidad` INT NOT NULL,
-  PRIMARY KEY (`id_carrito`),
-  INDEX `idx_id_cliente` (`id_cliente`),
-  CONSTRAINT `carrito_ibfk_3`
-    FOREIGN KEY (`id_cliente`)
-    REFERENCES `grappe1`.`cliente` (`id_cliente`)
-)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4;
-
-
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
 
 -- -----------------------------------------------------
 -- Table `grappe1`.`categoria`
@@ -123,26 +78,30 @@ CREATE TABLE IF NOT EXISTS `grappe1`.`categoria` (
   `Descripcion` TEXT NOT NULL,
   PRIMARY KEY (`id_categoria`))
 ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4;
-
+AUTO_INCREMENT = 20
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
 
 -- -----------------------------------------------------
--- Table `grappe1`.`contiene`
+-- Table `grappe1`.`cliente`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `grappe1`.`contiene` (
-  `id_producto` INT NOT NULL,
-  `id_carrito` INT NOT NULL,
-  INDEX `id_producto` (`id_producto` ASC, `id_carrito` ASC) VISIBLE,
-  INDEX `id_carrito` (`id_carrito` ASC) VISIBLE,
-  CONSTRAINT `contiene_ibfk_1`
-    FOREIGN KEY (`id_producto`)
-    REFERENCES `grappe1`.`producto` (`id_producto`),
-  CONSTRAINT `contiene_ibfk_2`
-    FOREIGN KEY (`id_carrito`)
-    REFERENCES `grappe1`.`carrito` (`id_carrito`))
+CREATE TABLE IF NOT EXISTS `grappe1`.`cliente` (
+  `id_cliente` INT NOT NULL AUTO_INCREMENT,
+  `Nombre` VARCHAR(50) NOT NULL,
+  `Apellido` VARCHAR(50) NOT NULL,
+  `Email` VARCHAR(50) NOT NULL,
+  `Contraseña` VARCHAR(255) NOT NULL,
+  `Saldo` INT NOT NULL,
+  `Numero Tarjeta` BIGINT NOT NULL,
+  `Titular` VARCHAR(100) NOT NULL,
+  `Año Vencimiento` INT NOT NULL,
+  `Mes Vencimiento` INT NOT NULL,
+  `CVV` INT NOT NULL,
+  PRIMARY KEY (`id_cliente`))
 ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4;
-
+AUTO_INCREMENT = 232
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
 
 -- -----------------------------------------------------
 -- Table `grappe1`.`pedido`
@@ -150,50 +109,41 @@ DEFAULT CHARACTER SET = utf8mb4;
 CREATE TABLE IF NOT EXISTS `grappe1`.`pedido` (
   `id_pedido` INT NOT NULL AUTO_INCREMENT,
   `id_cliente` INT NOT NULL,
-  `id_carrito` INT NOT NULL,
   `Calle` VARCHAR(100) NOT NULL,
-  `Numero_Exterior` INT NOT NULL,
+  `Numero Exterior` INT NOT NULL,
   `Colonia` VARCHAR(100) NOT NULL,
   `Municipio` VARCHAR(100) NOT NULL,
   `Estado` VARCHAR(100) NOT NULL,
-  `Codigo_Postal` INT NOT NULL,
+  `Codigo Postal` INT NOT NULL,
   `Pais` VARCHAR(50) NOT NULL,
   `Total` INT NOT NULL,
   `Fecha` DATE NOT NULL,
   PRIMARY KEY (`id_pedido`),
-  INDEX `idx_id_cliente` (`id_cliente`),
-  INDEX `idx_id_carrito` (`id_carrito`),
+  INDEX `id_cliente` (`id_cliente` ASC),
   CONSTRAINT `pedido_ibfk_1`
     FOREIGN KEY (`id_cliente`)
-    REFERENCES `grappe1`.`cliente` (`id_cliente`),
-  CONSTRAINT `pedido_ibfk_2`
-    FOREIGN KEY (`id_carrito`)
-    REFERENCES `grappe1`.`carrito` (`id_carrito`)
-)
+    REFERENCES `grappe1`.`cliente` (`id_cliente`))
 ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4;
-
-
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
 
 -- -----------------------------------------------------
--- Table `grappe1`.`pertenece`
+-- Table `grappe1`.`producto_has_categoria`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `grappe1`.`pertenece` (
+CREATE TABLE IF NOT EXISTS `grappe1`.`producto_has_categoria` (
   `id_producto` INT NOT NULL,
   `id_categoria` INT NOT NULL,
-  INDEX `idx_id_producto` (`id_producto`, `id_categoria`),
-  INDEX `idx_id_categoria` (`id_categoria`),
+  INDEX `id_producto` (`id_producto` ASC, `id_categoria` ASC),
+  INDEX `id_categoria` (`id_categoria` ASC),
   CONSTRAINT `pertenece_ibfk_1`
     FOREIGN KEY (`id_producto`)
     REFERENCES `grappe1`.`producto` (`id_producto`),
   CONSTRAINT `pertenece_ibfk_2`
     FOREIGN KEY (`id_categoria`)
-    REFERENCES `grappe1`.`categoria` (`id_categoria`)
-)
+    REFERENCES `grappe1`.`categoria` (`id_categoria`))
 ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4;
-
-
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
 
 -- -----------------------------------------------------
 -- Table `grappe1`.`review`
@@ -206,40 +156,61 @@ CREATE TABLE IF NOT EXISTS `grappe1`.`review` (
   `fecha_review` DATE NOT NULL,
   `rating` INT NOT NULL,
   PRIMARY KEY (`id_review`),
-  INDEX `idx_id_cliente` (`id_cliente`),
-  INDEX `idx_id_producto` (`id_producto`),
+  INDEX `id_cliente` (`id_cliente` ASC),
+  INDEX `id_producto` (`id_producto` ASC),
   CONSTRAINT `review_ibfk_1`
     FOREIGN KEY (`id_cliente`)
     REFERENCES `grappe1`.`cliente` (`id_cliente`),
   CONSTRAINT `review_ibfk_2`
     FOREIGN KEY (`id_producto`)
-    REFERENCES `grappe1`.`producto` (`id_producto`)
-)
+    REFERENCES `grappe1`.`producto` (`id_producto`))
 ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4;
-
-
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
 
 -- -----------------------------------------------------
--- Table `grappe1`.`tiene`
+-- Table `grappe1`.`pedido_has_producto`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `grappe1`.`tiene` (
+CREATE TABLE IF NOT EXISTS `grappe1`.`pedido_has_producto` (
   `cantidad` INT NOT NULL,
+  `producto` INT NOT NULL,
   `id_producto` INT NOT NULL,
   `id_pedido` INT NOT NULL,
-  INDEX `idx_id_producto` (`id_producto`, `id_pedido`),
-  INDEX `idx_id_pedido` (`id_pedido`),
+  INDEX `id_producto` (`id_producto` ASC, `id_pedido` ASC),
+  INDEX `id_pedido` (`id_pedido` ASC),
   CONSTRAINT `tiene_ibfk_1`
     FOREIGN KEY (`id_producto`)
     REFERENCES `grappe1`.`producto` (`id_producto`),
   CONSTRAINT `tiene_ibfk_2`
     FOREIGN KEY (`id_pedido`)
-    REFERENCES `grappe1`.`pedido` (`id_pedido`)
-)
+    REFERENCES `grappe1`.`pedido` (`id_pedido`))
 ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4;
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
 
-
+-- -----------------------------------------------------
+-- Table `grappe1`.`producto_carrito`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `grappe1`.`producto_carrito` (
+  `cliente_id_cliente` INT NOT NULL,
+  `producto_id_producto` INT NOT NULL,
+  `cantidad` INT NOT NULL,
+  PRIMARY KEY (`cliente_id_cliente`, `producto_id_producto`),
+  INDEX `fk_cliente_has_producto_producto1_idx` (`producto_id_producto` ASC),
+  INDEX `fk_cliente_has_producto_cliente1_idx` (`cliente_id_cliente` ASC),
+  CONSTRAINT `fk_cliente_has_producto_cliente1`
+    FOREIGN KEY (`cliente_id_cliente`)
+    REFERENCES `grappe1`.`cliente` (`id_cliente`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_cliente_has_producto_producto1`
+    FOREIGN KEY (`producto_id_producto`)
+    REFERENCES `grappe1`.`producto` (`id_producto`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
