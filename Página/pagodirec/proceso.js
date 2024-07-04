374b9bc117cda4c08da0bdbae3117bb2a46dc6b5
Aom29 <127160678+Aom29@users.noreply.github.com>
2024-07-04 08:07:55 -0600

    Proceso funcional

--- Página/pagodirec/proceso.js
+++ Página/pagodirec/proceso.js
@@ -89,6 +89,8 @@ function show(param_div_id){
 
 // BOTONES
 document.addEventListener('DOMContentLoaded', function() {
+    let emailLogin = localStorage.getItem("emailLogin");
+    console.log("Email de login: " + emailLogin);
     const cancelButton2 = document.querySelector('.cancel-button2');
     cancelButton2.addEventListener('click', function() {
         Swal.fire({
@@ -107,7 +109,7 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
     });
-
+    
     const cancelButton = document.querySelector('.cancel-button');
     cancelButton.addEventListener('click', function() {
         Swal.fire({
@@ -126,7 +128,7 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
     });
-
+    
     const cancelButton3 = document.querySelector('.cancel-button3');
     cancelButton3.addEventListener('click', function() {
         Swal.fire({
@@ -145,41 +147,33 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
     });
+    
+    let calle = "";
+    let numero = "";
+    let colonia = "";
+    let cp = "";
+    let municipio = "";
+    let estado = "";
+    let pais = "";
 
-    const saveButton3 = document.querySelector('.save-button3');
-    saveButton3.addEventListener('click', function() {
-        Swal.fire({
-            title: '¡Método de pago confirmado!',
-            icon: 'success',
-            confirmButtonColor: '#3085d6',
-            confirmButtonText: 'Aceptar'
-        }).then((result) => {
-            if (result.isConfirmed) {
-                // Mostrar la sección de Confirmar
-                show('Confirmar');
-            }
-        });
-    });
-
-    // Listener para el botón de guardar del primer contenedor
-    const saveButton2 = document.querySelector('.save-button2');
-    saveButton2.addEventListener('click', function() {
-        Swal.fire({
-            title: '¡Pedido realizado con éxito!',
-            text: 'El pedido ha sido procesado correctamente.',
-            icon: 'success',
-            confirmButtonColor: '#3085d6',
-            confirmButtonText: 'Aceptar'
-        }).then((result) => {
-            if (result.isConfirmed) {
-                // Mostrar la sección de Progreso
-                show('Progreso');
-            }
-        });
-    });
-
-    const saveButton = document.querySelector('.save-button');
-    saveButton.addEventListener('click', function() {
+    
+    // PUSE ESTO EN LUGAR DEL SAVE-BUTTON
+    
+    // Detectar cuando se envia el formulario y prevenir el comportamiento por defecto
+    const form = document.getElementById('datosCliente');
+    form.addEventListener('submit', function(event) {
+        // Evitar que se recargue la página
+        event.preventDefault();
+        // Guardar los valores de los campos en las variables correspondientes
+        calle = document.getElementById('calle').value;
+        numero = document.getElementById('numero').value;
+        colonia = document.getElementById('colonia').value;
+        cp = document.getElementById('cp').value;
+        municipio = document.getElementById('municipio').value;
+        estado = document.getElementById('estado').value;
+        pais = document.getElementById('pais').value;
+        console.log(calle, numero, colonia, cp, municipio, estado, pais);
+        // Mostrar mensaje de éxito
         Swal.fire({
             title: '¡Dirección guardada!',
             text: 'La Dirección ha sido procesado correctamente.',
@@ -193,6 +187,161 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
     });
+
+
+    // Para el método de pago
+    const saveButton3 = document.querySelector('.save-button3');
+    saveButton3.addEventListener('click', function() {
+        // LLENAR LA PÁGINA DE CONFIRMACIÓN
+        // Ajax para obtener los productos del carrito
+        url = "./phppago/obtenerCarrito_AX.php";
+        $.ajax({
+            url:url,
+            type: "POST",
+            data: {emailLogin:emailLogin},
+            cache: false,
+            success:(respAX)=>{
+                console.log(respAX);
+                let objRespAX = JSON.parse(respAX);
+                let mensaje = objRespAX.msj;
+                let exito = objRespAX.cod;
+                if(exito > 0){
+                    let productos = objRespAX.productos;
+                    let total = objRespAX.total;
+                    let totalDescuento = objRespAX.totalDescuento;
+                    let totalFinal = objRespAX.totalFinal;
+                    let carritoItems = document.querySelector('.carrito-items');
+                    carritoItems.innerHTML = "";
+                    for(let i = 0; i < productos.length; i++){
+                        let producto = productos[i];
+                        let divItem = document.createElement("div");
+                        divItem.classList.add("carrito-item");
+                        divItem.innerHTML = `
+                            <span class="carrito-item-titulo">${producto.nombre}</span>
+                            <img src="${producto.imagen}" width="80px" alt="">
+                            <div class="carrito-item-detalles">
+                                
+                                <div class="selector-cantidad">
+                                    <span class="carrito-item-titulo">Cantidad</span>
+                                    <input type="text" value="${producto.cantidad}" class="carrito-item-cantidad" disabled>
+                                    <span class="carrito-item-titulo">Precio</span>
+                                    <span class="carrito-item-precio">$${producto.precio}</span>
+                                </div>
+                                
+                                
+                            </div>
+                        `;
+                        carritoItems.appendChild(divItem);
+                    }
+
+                    // Parte derecha de la página
+                    let rightSide = document.querySelector('.right-side');
+                    rightSide.innerHTML = `
+                        <div class="top-right">
+                            <h5>Dirección</h5>
+                            <p>${calle} ${numero}, ${colonia}, ${municipio}, ${estado}, ${pais}</p>
+                        </div>
+                        <div class="bottom-right">
+                            <h5>Total</h5>
+                            <p class="total-amount">$${total}</p>
+                            <div class="discount">
+                                <span>- $${- totalDescuento}</span>
+                            </div>
+                            <h4 class="final-amount">$${totalFinal}</h4>
+                        </div>
+                    `;
+                }
+                else{
+                    Swal.fire({
+                        title: "Error al obtener carrito",
+                        text: mensaje,
+                        icon: "error",
+                    });
+                }
+            }
+        });
+
+        console.log(calle, numero, colonia, cp, municipio, estado, pais);
+        Swal.fire({
+            title: '¡Método de pago confirmado!',
+            icon: 'success',
+            confirmButtonColor: '#3085d6',
+            confirmButtonText: 'Aceptar'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                // Mostrar la sección de Confirmar
+                show('Confirmar');
+            }
+        });
+    });
+
+
+    // Listener para el botón de guardar del primer contenedor
+    // Para confirmar el pedido
+    const saveButton2 = document.querySelector('.save-button2');
+    saveButton2.addEventListener('click', function() {
+
+        // Ajax para enviar los datos del pedido
+        url = "./phppago/hacerPedidoSaldo_AX.php";
+        $.ajax({
+            url:url,
+            type: "POST",
+            data: {
+                emailLogin: emailLogin,
+                calle: calle,
+                numero: numero,
+                colonia: colonia,
+                municipio: municipio,
+                estado: estado,
+                cp: cp,
+                pais: pais
+            },
+            cache: false,
+            success:(respAX)=>{
+                console.log(respAX);
+                let objRespAX = JSON.parse(respAX);
+                let mensaje = objRespAX.msj;
+                let exito = objRespAX.cod;
+                if(exito > 0){
+                    Swal.fire({
+                        title: "Pedido enviado",
+                        text: "Tu pedido ha sido enviado correctamente",
+                        icon: "success",
+                        didDestroy:()=>{
+                            show("Progreso");
+                        }
+                    });
+                }
+                else{
+                    Swal.fire({
+                        title: "Error al generar pedido",
+                        text: mensaje,
+                        icon: "error",
+                    });
+                }
+            }
+        });
+
+    });
+
+
+    // const saveButton2 = document.querySelector('.save-button2');
+    // saveButton2.addEventListener('click', function() {
+    //     Swal.fire({
+    //         title: '¡Pedido realizado con éxito!',
+    //         text: 'El pedido ha sido procesado correctamente.',
+    //         icon: 'success',
+    //         confirmButtonColor: '#3085d6',
+    //         confirmButtonText: 'Aceptar'
+    //     }).then((result) => {
+    //         if (result.isConfirmed) {
+    //             // Mostrar la sección de Progreso
+    //             show('Progreso');
+    //         }
+    //     });
+    // });
+
+
 
 });
 
